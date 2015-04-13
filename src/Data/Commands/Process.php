4aece0c153ec82ee31c9d62e1c43e691496f65a6
Mikko Heikkinen <hawk@biomi.org>
2015-04-13 13:00:37 +0300

    name change

---
+++ src/Data/Commands/Process.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Data\Commands;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Formatter\OutputFormatterStyle;
+
+class Process extends Command {
+
+    protected function configure()
+    {   
+        $rows = 10;
+        $fileName = "demo.txt";
+
+        $this->setName("data:process")
+             ->setDescription("Process data from a file specified")
+             ->setDefinition(array(
+                      new InputOption('rows', 'r', InputOption::VALUE_OPTIONAL, 'Number of rows to handle', $rows),
+                      new InputOption('file', 'f', InputOption::VALUE_OPTIONAL, 'Name of datafile', $fileName)
+                ))
+             ->setHelp(<<<EOT
+Process data from a file specified
+
+Usage:
+
+<info>app/console data:process -r 10 -f data.txt</info>
+EOT
+);
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        // Styles
+        $header_style = new OutputFormatterStyle('white', 'green', array('bold'));
+        $output->getFormatter()->setStyle('header', $header_style);
+
+        // Options
+        $rows = intval($input->getOption('rows'));
+        $fileName  = $input->getOption('file');
+
+        if ($rows < 0)
+        {
+           throw new \InvalidArgumentException('Row count must be higher than zero.');
+        }
+
+        // Process
+        $output->writeln('<header>Processing data from ' . $fileName . '</header>');
+
+        $i = 0;
+        while ($i < $rows)
+        {
+            $output->writeln('<header>'.$i.'</header>');
+            $i++;
+        }
+
+        // Summary
+        $output->writeln('<header>Total rows = '.$i.' </header>');
+    }
+}
