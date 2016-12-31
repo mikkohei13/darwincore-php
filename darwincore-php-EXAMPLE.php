62871c0a943bcf1c5d50436a004d70021933f8fc
Mikko Heikkinen <hawk@biomi.org>
2016-12-31 23:19:17 +0200

    example settings file

---
+++ darwincore-php-EXAMPLE.php
@@ -0,0 +1,8 @@
+<?php
+
+// Fill this with your own connection settings and
+// place this file one level below darwincore-php's root directory
+
+$hosts = ['http://USERNAME:PASSWORD@SERVER-NAME:9200'];
+
+
